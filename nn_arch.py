0d68e530e0ac8511c6af23e45901a778bc1f53c3
yangjc <bupt_yjc@163.com>
2019-01-16 21:16:52 +0800

    fix trm

--- nn_arch.py
+++ nn_arch.py
@@ -11,36 +11,46 @@ class Trm(nn.Module):
         vocab_num, embed_len = embed_mat.size()
         self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
         self.pos = pos_mat
-        self.head, self.stack = head, stack
-        self.querys, self.keys, self.vals = [[[nn.Linear(embed_len, 200)] * head] * stack] * 3
-        self.merges = [nn.Linear(200 * head, 200)] * stack
-        self.lals = [nn.Sequential(nn.Linear(200, 200),
-                                   nn.ReLU(),
-                                   nn.Linear(200, 200))] * stack
+        self.stack = stack
+        self.encodes = nn.ModuleList([TrmEncode(embed_len, head) for _ in range(stack)])
         self.dl = nn.Sequential(nn.Dropout(0.2),
                                 nn.Linear(200, class_num))
-
-    def mul_att(self, i, x):
-        c = list()
-        for j in range(self.head):
-            q, k, v = self.querys[i][j](x), self.keys[i][j](x), self.vals[i][j](x)
-            scale = math.sqrt(k.size(-1))
-            d = torch.matmul(q, k.permute(0, 2, 1)) / scale
-            a = F.softmax(d, dim=-1)
-            c_i = torch.matmul(a, v)
-            c.append(c_i)
-        x = torch.cat(c, dim=-1)
-        return self.merges[i](x)
 
     def forward(self, x):
         x = self.embed(x)
         x = x + self.pos
         for i in range(self.stack):
-            r = x
-            x = self.mul_att(i, x)
-            x = F.layer_norm(x + r, x.size()[1:])
-            r = x
-            x = self.lals[i](x)
-            x = F.layer_norm(x + r, x.size()[1:])
-        x = x[:, -1, :]
+            x = self.encodes[i](x)
+        x = x[:, 0, :]
         return self.dl(x)
+
+
+class TrmEncode(nn.Module):
+    def __init__(self, embed_len, head):
+        super(TrmEncode, self).__init__()
+        self.head = head
+        self.qry = nn.Linear(embed_len, 200 * head)
+        self.key = nn.Linear(embed_len, 200 * head)
+        self.val = nn.Linear(embed_len, 200 * head)
+        self.fuse = nn.Linear(200 * head, 200)
+        self.lal = nn.Sequential(nn.Linear(200, 200),
+                                 nn.ReLU(),
+                                 nn.Linear(200, 200))
+
+    def mul_att(self, x):
+        q = self.qry(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        k = self.key(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        v = self.val(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        d = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(k.size(-1))
+        a = F.softmax(d, dim=-1)
+        c = torch.matmul(a, v).transpose(1, 2)
+        c = c.contiguous().view(c.size(0), c.size(1), -1)
+        return self.fuse(c)
+
+    def forward(self, x):
+        r = x
+        x = self.mul_att(x)
+        x = F.layer_norm(x + r, x.size()[1:])
+        r = x
+        x = self.lal(x)
+        return F.layer_norm(x + r, x.size()[1:])
