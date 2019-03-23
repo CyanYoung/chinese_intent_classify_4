b9c47643618f50da78300a1a04801a64deb09cfc
yangjc <bupt_yjc@163.com>
2019-03-23 11:32:03 +0800

    add f1

--- eval.py
+++ eval.py
@@ -3,7 +3,7 @@ import pickle as pk
 import torch
 import torch.nn.functional as F
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 from build import tensorize
 
@@ -23,6 +23,10 @@ with open(path_sent, 'rb') as f:
 with open(path_label, 'rb') as f:
     labels = pk.load(f)
 
+class_num = len(ind_labels)
+
+paths = {'trm': 'stat/metric/trm.csv'}
+
 
 def test(name, sents, labels):
     sents, labels = tensorize([sents, labels], device)
@@ -31,7 +35,14 @@ def test(name, sents, labels):
         model.eval()
         probs = F.softmax(model(sents), dim=1)
     preds = torch.max(probs, dim=1)[1]
-    print('\n%s %s %.2f\n' % (name, 'acc:', accuracy_score(labels, preds)))
+    precs = precision_score(labels, preds, average=None)
+    recs = recall_score(labels, preds, average=None)
+    with open(map_item(name, paths), 'w') as f:
+        f.write('label,prec,rec' + '\n')
+        for i in range(class_num):
+            f.write('%s,%.2f,%.2f\n' % (ind_labels[i], precs[i], recs[i]))
+    f1 = f1_score(labels, preds, average='weighted')
+    print('\n%s f1: %.2f - acc: %.2f\n' % (name, f1, accuracy_score(labels, preds)))
     for text, label, pred in zip(texts, labels.numpy(), preds.numpy()):
         if label != pred:
             print('{}: {} -> {}'.format(text, ind_labels[label], ind_labels[pred]))
